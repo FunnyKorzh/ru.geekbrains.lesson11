ea0ee462d1e9140c4a4a77155be20ef9f6b22d42
Dmitriy <79575923+FunnyKorzh@users.noreply.github.com>
2021-03-31 10:29:55 +0300

    Add files via upload

---
+++ Orange.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.lesson11;
+
+public class Orange extends Fruit {
+
+    Orange() {
+        super(2.8f);
+    }
+
+    @Override
+    public String toString(){
+        return "Апельсины";
+    }
+
+}
