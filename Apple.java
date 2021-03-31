f1a065fb21a6398a6920df3adcffcbb56468c9df
Dmitriy <79575923+FunnyKorzh@users.noreply.github.com>
2021-03-31 10:29:09 +0300

    Add files via upload

---
+++ Apple.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.lesson11;
+
+public class Apple extends Fruit {
+
+    Apple() {
+        super(3.0f);
+    }
+
+    @Override
+    public String toString(){
+        return "Яблоки";
+    }
+}
